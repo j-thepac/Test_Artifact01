948df9acb2b8518a3dee870fbd53c0922f09e69f
thepac <j_thepac@yahoo.com>
2016-02-21 16:54:53 +0530

    initial commit

---
+++ src/test/java/com/gradletest/gradletest.java
@@ -0,0 +1,15 @@
+package com.gradletest;
+
+//import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Guest on 2/21/2016.
+ */
+public class gradletest {
+
+    @Test
+    public void test1(){
+        System.out.print("hi");
+    }
+}
