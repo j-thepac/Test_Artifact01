b978279a1e4949e5d7c814962809ed6378ae48d4
Guest <Guest@deepak-PC>
2016-03-07 21:36:27 +0530

    - commiting driver class

---
+++ src/test/java/com/library/Driver_class.java
@@ -0,0 +1,39 @@
+package com.library;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Driver_class {
+	
+
+	private WebDriver driver;
+	Driver_class driver_class;
+	private int iPage_loadtimeout;
+	private int implicitlyWait;
+	
+	public void Driver_class(String sBrowser,int iPage_loadtimeout,int implicitlyWait)
+	{
+		if (sBrowser.toLowerCase()=="ie")
+			this.driver=new InternetExplorerDriver();
+		else if (sBrowser.toLowerCase()=="chromedriver")
+			this.driver=new ChromeDriver();
+		else
+			this.driver=new FirefoxDriver();
+		
+		
+		System.setProperty("webdriver.ie.driver",GRADLE_HOME +"\\IEDriverServer.exe");
+		System.setProperty("webdriver.ie.logfile", "F:\\Study\\IEDriverServer.log");
+		
+		this.driver.manage().timeouts().pageLoadTimeout(iPage_loadtimeout, TimeUnit.SECONDS); 
+		this.driver.manage().timeouts().implicitlyWait(implicitlyWait,TimeUnit.SECONDS);
+		
+		
+	}
+	
+
+
+}
